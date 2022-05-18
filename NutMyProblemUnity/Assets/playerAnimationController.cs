1d14e2984a4a80ebbbdc7cd280aac123f8faadd4
BauaMomo <99999855+BauaMomo@users.noreply.github.com>
2022-05-18 21:54:30 +0200

    Update playerAnimationController.cs

--- NutMyProblemUnity/Assets/playerAnimationController.cs
+++ NutMyProblemUnity/Assets/playerAnimationController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class playerAnimationController : MonoBehaviour
 {
+    playerController playerController;
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerController = GetComponent<playerController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
