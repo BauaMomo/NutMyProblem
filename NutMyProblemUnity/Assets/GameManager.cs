aa436e408193b2bacb0867fe64e5110ec50a9a43
BauaMomo <99999855+BauaMomo@users.noreply.github.com>
2022-06-02 14:55:27 +0200

    NUT-73 Added Player Health Text and Current Weapon Text

--- NutMyProblemUnity/Assets/GameManager.cs
+++ NutMyProblemUnity/Assets/GameManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    GameObject player;
+    public GameObject player;
     GameObject enemy;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         player = Instantiate(Resources.Load("prefabs/Player") as GameObject);
         player.transform.position = new Vector2(0,0);
