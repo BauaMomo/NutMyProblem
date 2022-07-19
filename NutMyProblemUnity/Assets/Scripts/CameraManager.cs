dcbe6fe69817a3a473815df569c4ae8e33ec65ec
BauaMomo <99999855+BauaMomo@users.noreply.github.com>
2022-07-19 15:09:39 +0200

    NUT-82 Camera Movement
    -Camera follows a box that gets pushed by the player
    -Smooth acceleration of camera with MoveTowards()

--- NutMyProblemUnity/Assets/Scripts/CameraManager.cs
+++ NutMyProblemUnity/Assets/Scripts/CameraManager.cs
@@ -6,16 +6,22 @@ public class CameraManager : MonoBehaviour
 {
     GameObject player;
     GameManager gameManager;
+    GameObject CameraPushBox;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         player = gameManager.player;
+
+        CameraPushBox = Instantiate(Resources.Load("Prefabs/CameraPushBox") as GameObject);
+        CameraPushBox.transform.position = player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y +2, -10);
+        Vector3 newCamPosition = new Vector3(CameraPushBox.transform.position.x, CameraPushBox.transform.position.y + 3, -10);                                 //Moves the camera towards the CameraPushBox
+        transform.position = Vector3.MoveTowards(transform.position, newCamPosition, Vector3.Distance(transform.position, newCamPosition) / 5 + 0.3f);         //CameraPushBox is a box of colliders, gets pushed around by the player
     }
 }
